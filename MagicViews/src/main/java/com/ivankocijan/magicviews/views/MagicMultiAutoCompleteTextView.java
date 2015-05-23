205ec7df5929a624ba88136af3d5fa536d41e93f
ikocijan <ikocijan0@gmail.com>
2015-05-23 11:51:11 +0200

    Added MagicMultiAutoCompleteTextView

---
+++ MagicViews/src/main/java/com/ivankocijan/magicviews/views/MagicMultiAutoCompleteTextView.java
@@ -0,0 +1,37 @@
+package com.ivankocijan.magicviews.views;
+
+import com.ivankocijan.magicviews.R;
+import com.ivankocijan.magicviews.utils.FontUtils;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatMultiAutoCompleteTextView;
+import android.util.AttributeSet;
+
+/**
+ * @author Koc
+ *         ivan.kocijan@infinum.hr
+ * @since 23.05.15.
+ */
+public class MagicMultiAutoCompleteTextView extends AppCompatMultiAutoCompleteTextView {
+
+    public MagicMultiAutoCompleteTextView(Context context) {
+        this(context, null);
+    }
+
+    public MagicMultiAutoCompleteTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.autoCompleteTextViewStyle);
+    }
+
+    public MagicMultiAutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+
+        FontUtils.setTypeface(getContext(), attrs, this);
+
+    }
+
+
+}
