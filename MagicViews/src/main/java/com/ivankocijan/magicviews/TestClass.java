ad610b0af608d0957117ce3445d4e2651b42edb5
Ivan Kocijan <ivan.kocijan@infinum.hr>
2016-10-09 21:58:00 +0200

    Added test classes

---
+++ MagicViews/src/main/java/com/ivankocijan/magicviews/TestClass.java
@@ -0,0 +1,28 @@
+package com.ivankocijan.magicviews;
+
+/**
+ * @author Koc
+ *         ivan.kocijan@infinum.hr
+ * @since 09/10/16
+ */
+
+public class TestClass {
+
+    public static String CONSTANT = "CONSTANt";
+
+
+    public TestClass() {
+    }
+
+    public void evaluate(){
+        throw new NullPointerException("Null");
+    }
+
+    private boolean returnTrue () {
+        if(true) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ MagicViews/src/main/java/com/ivankocijan/magicviews/TestClass2.java
@@ -0,0 +1,11 @@
+package com.ivankocijan.magicviews;
+
+/**
+ * @author Koc
+ *         ivan.kocijan@infinum.hr
+ * @since 09/10/16
+ */
+
+public class TestClass2 {
+
+}
